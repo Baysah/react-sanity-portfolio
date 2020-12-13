62adacf8c4deaccd4df34201f4f564734960c0fb
Baysah Korti <baysahkorti@gmail.com>
2020-12-13 06:40:06 -0500

    Changed greetings on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
         <main>
                 <img src={bgImage} alt="Mountain View" className="absolute object-cover w-full h-full"/>
                 <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                    <h1 className="text-6xl text-blue-500 cursive leading-none lg:leading-snug home-name ">Aloha, I'm Baysah Korti.</h1>
+                    <h1 className="text-6xl text-blue-500 cursive leading-none lg:leading-snug home-name ">Hello, I'm Baysah Korti.</h1>
                 </section>
             </main>
     )
